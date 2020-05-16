7b89d89bac9d3cf5a3501a5b5065c5a64394cf4a
Nathan Jaber <nathanjaber2014@gmail.com>
2020-05-16 16:34:11 -0300

    Teste Converter para uma nota de Dez

--- src/test/java/org/codingdojo/DinheiroTest.java
+++ src/test/java/org/codingdojo/DinheiroTest.java
@@ -34,8 +34,22 @@ public class DinheiroTest {
 
     @Test
     public void testConverterParaUmaNotaDeDois () {
-        List<Nota> notas = new Dinheiro(2). converterParaNotas();
+        List<Nota> notas = new Dinheiro(2).converterParaNotas();
         assertTrue(notas.get(0).equals(new Nota(2)));
+        assertTrue(notas.size() == 1);
+    }
+
+    @Test
+    public void testConverterParaUmaNotaDeCinco() {
+        List<Nota> notas = new Dinheiro(5).converterParaNotas();
+        assertTrue(notas.get(0).equals(new Nota(5)));
+        assertTrue(notas.size() == 1);
+    }
+
+    @Test
+    public void testConverterParaUmaNotaDeDez() {
+        List<Nota> notas = new Dinheiro(10).converterParaNotas();
+        assertTrue(notas.get(0).equals(new Nota(10)));
         assertTrue(notas.size() == 1);
     }
 }
